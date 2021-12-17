678676c9715b6d00d12a8ee1c12f098abbd92dc6
Aliaksandr Belavus <58934324+AliaksandrBelavus@users.noreply.github.com>
2021-12-17 11:32:14 +0200

    Delete classificationLevel.js

--- JS/models/classificationLevel.js
+++
@@ -1,8 +0,0 @@
-const CLASSIFICATION_LEVEL = {
-  UNCLASSIFIED: "Unclassified",
-  CONFIDENTIAL: "Confidential",
-  SECRET: "Secret",
-  TOP_SECRET: "Top secret",
-};
-
-module.exports = { CLASSIFICATION_LEVEL };
